09e44d6390d8a0aac0d2ab24bb050ca130dd9eaa
devintang <yonghua_t@qq.com>
2015-12-14 15:12:49 +0800

    测试提交代码

---
+++ testavro/src/main/java/org/cmcc/iot/avro/Test.java
@@ -0,0 +1,13 @@
+package org.cmcc.iot.avro;
+
+/**
+ *
+ * Created by Administrator on 2015/12/14.
+ */
+public class Test {
+    private int a;
+
+    public static void main(String[] args) {
+        System.out.println("hello-world");
+    }
+}

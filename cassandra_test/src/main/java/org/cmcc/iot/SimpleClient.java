49227d721609c71d7931ee6070ecefa49f995b36
devintang <yonghua_t@qq.com>
2015-12-15 17:03:27 +0800

    cassandra cql test

--- cassandra_test/src/main/java/org/cmcc/iot/SimpleClient.java
+++ cassandra_test/src/main/java/org/cmcc/iot/SimpleClient.java
@@ -1,8 +1,6 @@
 package org.cmcc.iot;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Host;
-import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.*;
 
 /**
  * test connect cassandra cluster
@@ -10,6 +8,11 @@ import com.datastax.driver.core.Metadata;
  */
 public class SimpleClient {
     private Cluster cluster;
+    private Session session;
+
+    public Session getSession() {
+        return this.session;
+    }
 
     /**
      * connect cassandra cluster
@@ -19,6 +22,7 @@ public class SimpleClient {
         cluster = Cluster.builder()
                 .addContactPoint(node)
                 .build();
+//        cluster.connect("test");
         Metadata metadata = cluster.getMetadata();
         System.out.printf("Connected to cluster: %s\n",
                 metadata.getClusterName());
@@ -26,6 +30,80 @@ public class SimpleClient {
             System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
                     host.getDatacenter(), host.getAddress(), host.getRack());
         }
+
+        session = cluster.connect();
+    }
+
+    /**
+     * create keyspace and table
+     */
+    public void createSchema() {
+        session.execute("CREATE KEYSPACE IF NOT EXISTS simplex WITH replication " +
+                "= {'class':'SimpleStrategy', 'replication_factor':2};");
+        session.execute(
+                "CREATE TABLE IF NOT EXISTS simplex.songs (" +
+                        "id uuid PRIMARY KEY," +
+                        "title text," +
+                        "album text," +
+                        "artist text," +
+                        "tags set<text>," +
+                        "data blob" +
+                        ");");
+        session.execute(
+                "CREATE TABLE IF NOT EXISTS simplex.playlists (" +
+                        "id uuid," +
+                        "title text," +
+                        "album text, " +
+                        "artist text," +
+                        "song_id uuid," +
+                        "PRIMARY KEY (id, title, album, artist)" +
+                        ");");
+    }
+
+    /**
+     * load data into db
+     */
+    public void loadData() {
+        session.execute(
+                "INSERT INTO simplex.songs (id, title, album, artist, tags) " +
+                        "VALUES (" +
+                        "756716f7-2e54-4715-9f00-91dcbea6cf50," +
+                        "'La Petite Tonkinoise'," +
+                        "'Bye Bye Blackbird'," +
+                        "'Joséphine Baker'," +
+                        "{'jazz', '2013'})" +
+                        ";");
+        session.execute(
+                "INSERT INTO simplex.playlists (id, song_id, title, album, artist) " +
+                        "VALUES (" +
+                        "2cc9ccb7-6221-4ccb-8387-f22b6a1b354d," +
+                        "756716f7-2e54-4715-9f00-91dcbea6cf50," +
+                        "'La Petite Tonkinoise'," +
+                        "'Bye Bye Blackbird'," +
+                        "'Joséphine Baker'" +
+                        ");");
+    }
+
+    /**
+     * query data from db
+     */
+    public void querySchema() {
+        ResultSet results = session.execute("SELECT * FROM simplex.playlists " +
+                "WHERE id = 2cc9ccb7-6221-4ccb-8387-f22b6a1b354d;");
+        System.out.println(String.format("%-30s\t%-20s\t%-20s\n%s", "title", "album", "artist",
+                "-------------------------------+-----------------------+--------------------"));
+        for (Row row : results) {
+            System.out.println(String.format("%-30s\t%-20s\t%-20s", row.getString("title"),
+                    row.getString("album"),  row.getString("artist")));
+        }
+        System.out.println();
+    }
+
+    /**
+     * drop keyspace
+     */
+    public void dropKeyspace() {
+        session.execute("DROP KEYSPACE simplex;");
     }
 
     /**
@@ -37,7 +115,10 @@ public class SimpleClient {
 
     public static void main(String[] args) {
         SimpleClient client = new SimpleClient();
-        client.connect("127.0.0.1");
+        client.connect("172.19.3.16");
+        client.createSchema();
+        client.loadData();
+        client.querySchema();
         client.close();
     }
 
